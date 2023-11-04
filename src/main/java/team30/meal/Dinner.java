a8d3bef7dec1ada8ed5a209bf9c44570bc003a34
GiantDwarfRen <114264732+GiantDwarfRen@users.noreply.github.com>
2023-11-03 20:45:18 -0700

    Implement MealType and Setup test file hierarchy

---
+++ src/main/java/team30/meal/Dinner.java
@@ -0,0 +1,7 @@
+package team30.meal;
+
+public class Dinner implements MealType{
+    static final private String MEAL_TYPE = "Dinner";
+
+    public String getMealType() {return MEAL_TYPE;}
+}
---
+++ src/main/java/team30/meal/Lunch.java
@@ -0,0 +1,7 @@
+package team30.meal;
+
+public class Lunch implements MealType{
+    static final private String MEAL_TYPE = "Lunch";
+
+    public String getMealType() {return MEAL_TYPE;}
+}
