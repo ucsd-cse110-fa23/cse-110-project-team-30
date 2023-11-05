1c52caf1668bed345b7204f353b7269d6559a71d
StickonFire <70039286+StickonFire@users.noreply.github.com>
2023-11-04 17:54:19 -0700

    Added mock classes for IChatGPT and IWhisper
    Added and implemented 2 classes, both mocks for interfaces:
     - the MockWhisper class mocks the IWhisper interface. It returns a set value to any input.
     - the MockChatGPT class mocks the IChatGPT interface. It returns a set value to any input.

---
+++ src/MockChatGPT.java
@@ -0,0 +1,23 @@
+import java.nio.file;
+
+/**
+ * A mock version of IChatGPT classes.
+ * Provides an already set text when given information to convert into a recipe.
+ */
+public class MockChatGPT implements IChatGPT {
+    String result;
+
+    MockChatGPT(String result){
+        this.result = result;
+    }
+    
+    /**
+     * Given a type of meal and ingredients, simply returns the value of result.
+     * @param type - type of the meal to create
+     * @param ingredients - type of ingredients to add
+     * @return the value of result
+     */
+    public String generateMeal(MealType type, String ingredients){
+        return this.result;
+    }
+}
---
+++ src/MockWhisper.java
@@ -0,0 +1,22 @@
+import java.nio.file;
+
+/**
+ * A mock version of IWhisper classes.
+ * Provides an already set text when given an audio file to decipher.
+ */
+public class MockWhisper implements IWhisper {
+    String result;
+
+    MockWhisper(String result){
+        this.result = result;
+    }
+
+    /**
+     * Given an audio file to decipher into a text, return a given result.
+     * @param input - audio file to decipher into a text
+     * @return the value of result
+     */
+    public String getResults(File input){
+        return result;
+    }
+}
