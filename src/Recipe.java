3639e50bf2b9e2b208adaee25e0045aec589cb90
StickonFire <70039286+StickonFire@users.noreply.github.com>
2023-11-04 21:53:39 -0700

    Create Recipe.java
    Added a new Recipe class, which represents a recipe. It has a type, name, ingredients, and steps.

---
+++ src/Recipe.java
@@ -0,0 +1,40 @@
+/**
+ * Representation of a recipe.
+ */
+public class Recipe {
+    MealType mealType;
+    String name;
+    String ingredients;
+    String steps;
+
+    Recipe(MealType mealType, String name, String ingredients, String steps){
+        this.mealType = mealType;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+    }
+
+    MealType getMealType(){
+        return this.mealType;
+    }
+
+    String getsteps(){
+        return this.steps;
+    }
+
+    String getIngredients(){
+        return this.ingredients;
+    }
+
+    String getName(){
+        return this.name;
+    }
+
+    void setSteps(String steps){
+        this.steps = steps;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+}
