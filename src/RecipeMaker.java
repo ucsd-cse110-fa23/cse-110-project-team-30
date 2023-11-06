1778f22759c86c8167ff1cf12a90df76a6f148e6
StickonFire <70039286+StickonFire@users.noreply.github.com>
2023-11-05 21:27:29 -0800

    Added RecipeMaker method skeletons
    Added skeletons for the following RecipeMaker methods:
     - getIngredients
     - getMealType
     - getInstructions
     - createRecipe
    
    Also fully implemented the following methods:
     - constructor
     - combineIntoRecipe

--- src/RecipeMaker.java
+++ src/RecipeMaker.java
@@ -1,4 +1,4 @@
-import team30.meal.*;
+import team30.meal.MealType;
 import openai_classes.*;
 
 import java.io.File;
@@ -13,4 +13,60 @@ import java.io.File;
 public class RecipeMaker {
     IWhisper whisper;
     IChatGPT chatGPT;
+
+    RecipeMaker(IWhisper whisper, IChatGPT chatGPT){
+        this.whisper = whisper;
+        this.chatGPT = chatGPT;
+    }
+
+    /**
+     * Given an audio file, makes a call to Whisper to get the ingredients spoken in the file.
+     * @param file - audio file to get the list of ingredients from.
+     * @return a string containing the speech within the given audio file, expected to be a list of ingredients.
+     */
+    private String getIngredients(File file){
+        return null;
+    }
+
+    /**
+     * Given an audio file, makes a call to Whisper to get the Meal Type.
+     * If a valid mealType is named, it is returned. Otherwise null is returned.
+     * @param file - audio file to convert to text
+     * @return the mealType that matches what's said in the file, or null if the word generated is invalid.
+     */
+    private MealType getMealType(File file){
+        return null;
+    }
+
+    /**
+     * Given the mealType and ingredients, calls ChatGPT to generate instructions.
+     * @param mealType - the type of meal to be created by ChatGPT
+     * @param ingredients - the type of ingredients to be used in the recipe created by ChatGPT.
+     * @return a string containing step by step instructions to create the recipe.
+     */
+    private String getInstructions(String mealType, String ingredients){
+        return null;
+    }
+
+    /**
+     * Given the mealType, title, ingredients, and instructions, creates a new recipe class with fields matching the arguements.
+     * @param mealType - intended value for the mealType
+     * @param title - intended value for the title.
+     * @param ingredients - intended value for the ingredients
+     * @param instructions - intended value for the instructions.
+     * @return a recipe with fields matching the arguments given.
+     */
+    Recipe combineIntoRecipe(MealType mealType, String title, String ingredients, String instructions){
+        return new Recipe(mealType, title, ingredients, instructions);
+    }
+
+    /**
+     * Given two files, creates a recipe out of the information in both files.
+     * @param mealTypeFile - an audio file containing a spoken type of meal.
+     * @param ingredientsFile - an audio file containing a spoken list of ingredients.
+     * @return a recipe of the meal type spoken in mealTypeFile and of ingredients spoken in ingredientsFile.
+     */
+    Recipe createRecipe(File mealTypeFile, File ingredientsFile){
+        return null;
+    }
 }
