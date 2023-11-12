1c4a38b95085725b319cb285734f240afecafc19
GiantDwarfRen <114264732+GiantDwarfRen@users.noreply.github.com>
2023-11-12 15:36:02 -0800

    Test more of RecipeList

--- src/test/java/team30/recipeList/RecipeListTest.java
+++ src/test/java/team30/recipeList/RecipeListTest.java
@@ -3,31 +3,51 @@ package team30.recipeList;
 import team30.recipeList.RecipeList;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.objenesis.instantiator.sun.MagicInstantiator;
 
+import javafx.beans.binding.ObjectBinding;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.text.*;
+import javafx.scene.control.TextField;
 
 class mockRecipe extends HBox {
     private String index;
     private String recipe_title;
+    private String mealtype;
+    private String ingredients;
+    private ArrayList<String> steps;
 
-    mockRecipe(String s) {
-        recipe_title = s;
+    mockRecipe() {
+        recipe_title = "example";
     }    
+
+    mockRecipe(String name, String ingredients, ArrayList<String> steps, String mealtype) {
+        this.recipe_title = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.mealtype = mealtype;
+    }
 
     public String getIndex() {return index;}
     public String getRecipeTitle() {return recipe_title;}
     public void setIndex(String s) {index = s;}
+    public String getMealType() {return mealtype;}
+    public String getIngredients() {return ingredients;}
+    public ArrayList<String> getSteps() {return steps;}
 }
 
 class mockList extends VBox {
@@ -50,21 +70,36 @@ class mockList extends VBox {
 
 class mockButton {
     public void fire(mockList ml, String s) {
-        mockRecipe recipe;
-        if (s == "") recipe = new mockRecipe("example");
-        else recipe = new mockRecipe(s);
+        mockRecipe recipe = new mockRecipe();
         ml.getChildren().add(recipe);
         ml.updateTaskIndices();
         System.out.println(recipe.getRecipeTitle());
     }
 }
 
+class mockHeader {
+    public String titleText;
+    public mockButton addButton;
+
+    mockHeader() {
+        titleText = "PantryPal";
+        addButton = new mockButton();
+    }
+    public String getTitleText() {return titleText;}
+
+    public mockButton getAddButton() {
+        return addButton;
+    }
+}
+
 public class RecipeListTest {
     private mockList recipe_list;
+    private mockHeader header;
 
     @BeforeEach
     void setUp() {
         recipe_list = new mockList();
+        header = new mockHeader();
     }
 
     @Test
@@ -97,7 +132,7 @@ public class RecipeListTest {
     }
 
     @Test
-    void testRecipeConstructor() {
+    void testRecipeDefaultConstructor() {
         recipe_list.getButton().fire(recipe_list, "");
         String title = "";
         for (int i = 0; i < recipe_list.getChildren().size(); i++) {
@@ -110,8 +145,27 @@ public class RecipeListTest {
     }
 
     @Test
+    void testRecipeConstructorWithParameters() {
+        String recipe_name = "Noodle";
+        String meal_type = "Dinner";
+        String ingredients = "Eggs, noodle, water...";
+        ArrayList<String> steps = new ArrayList<>();
+        steps.add(new String("Step 1"));
+        steps.add(new String("Step 2"));
+        steps.add(new String("Step 3"));
+
+        mockRecipe recipe = new mockRecipe(recipe_name, ingredients, steps, meal_type);
+        assertEquals(recipe_name, recipe.getRecipeTitle());
+        assertEquals(ingredients, recipe.getIngredients());
+        for (int i = 0; i < steps.size(); ++i) {
+            assertEquals(steps.get(i), recipe.getSteps().get(i));
+        }
+        assertEquals(meal_type, recipe.getMealType());
+    }
+
+    @Test
     void testSetUpRecipeTitle() {
-        String testTitle = "My Recipe Name";
+        String testTitle = "example";
         recipe_list.getButton().fire(recipe_list, testTitle);
         String title = "";
         for (int i = 0; i < recipe_list.getChildren().size(); i++) {
@@ -121,5 +175,14 @@ public class RecipeListTest {
             }
         }
         assertEquals(testTitle, title);
+    }
+
+    @Test
+    void testHeaderTitle() {
+        assertEquals("PantryPal", header.titleText);
+    }
+
+    @Test void testHeaderInitialization() {
+        assertNotEquals(null, header.addButton);
     }
 }
