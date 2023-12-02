5aa6e0664fed4a38d79e94dcf1d0aa312e0ebe5b
StickonFire <70039286+StickonFire@users.noreply.github.com>
2023-12-02 14:47:00 -0800

    Removed IImageManager
    Removed the abstract class IImageManager so that the class ImageManager can be made to have mostly static methods.

---
+++ src/main/java/team30/recipeList/ImageManager.java
@@ -0,0 +1,73 @@
+package team30.recipeList;
+
+import javafx.scene.image.Image;
+import java.io.File;
+import java.net.URI;
+
+public class ImageManager{
+    DallE dallE = new DallE();
+
+
+    public static final String path = "";
+
+    //TODO: Once the Server is cleaned up, have this instead make a call to the server to generate the image.
+
+    /**
+     * Creates an image with the given name if the name is clear.
+     * If not, a new path will be made to hold the image.
+     * @param name - the expected name for the image
+     * @return - a path in the files to the image
+     */
+    public String generateImage(String name){
+        String imgurl = createUniqueURI(name);
+        try{
+            dallE.generateImage(name, imgurl);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    /**
+     * Meant to ensure that a given path is good for use.
+     * If the path does not exist, or the regenerate flag is true,
+     * a new Image will be created and a new path pointing to it added.
+     * @param path - expected path of the Image
+     * @param name - name to use when generating a path IF a new path must be generated
+     * @param regenerate - flag that sets whether to regenerate the image or not.
+     * @return the original path, if valid, and a path to a new image if not.
+     */
+    public String ensurePathValid(String path, String name, boolean regenerate){
+        String imgurl = path;
+        if(regenerate || !(new File(imgurl).exists())){
+            imgurl = this.generateImage(name);
+        }
+        return imgurl;
+    }
+    
+    /**
+     * Attempts to get an image in the given path.
+     * It is recommended to run ensurePathValid first to ensure that the path does have an image.
+     * @param path - path of the image.
+     * @return image in the given path.
+     */
+    public Image getImage(String path){
+        File file = new File(path);
+        URI fileURI = file.toURI();
+        return new Image(fileURI.toString());
+    }
+    
+    /**
+     * Creates a unique URI for the 
+     * @param name - the expected name for the URI
+     * @return the path of the URI as a String
+     */
+    public String createUniqueURI(String name){
+        String url = name + ".png";
+        int counter = 0;
+        while((new File(url)).exists())
+            url = name + "(" + (counter++) + ")" + ".png";
+        return url;
+    }
+}
---
+++ src/test/java/team30/recipeList/ImageManagerTest.java
@@ -0,0 +1,37 @@
+package team30.recipeList;
+
+import team30.recipeList.RecipeList;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+import org.objenesis.instantiator.sun.MagicInstantiator;
+
+import javafx.beans.binding.ObjectBinding;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import java.util.ArrayList;
+import team30.mock.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import javafx.application.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+
+public class ImageManagerTest{
+    
+}
