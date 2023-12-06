e0531aaa9f46eff9785bce8e3cdd97205373b797
Lisa Liu <44093048+lisasiliu@users.noreply.github.com>
2023-12-05 23:51:21 -0800

    Create mockWhisper.java

---
+++ src/test/java/team30/mock/apis/mockWhisper.java
@@ -0,0 +1,12 @@
+package team30.mock.apis;
+
+import team30.server.Whisper;
+
+public class mockWhisper extends Whisper {
+    public void setInputFile() {
+        return;
+    }
+    public String run() {
+        return "lunch";
+    }
+}
